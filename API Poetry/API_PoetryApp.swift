d54fd057848fcf7ed383353a86dea4884899fc19
Maya Krishnan <mayark527@gmail.com>
2026-02-27 10:36:29 -0600

    Initial Commit

---
+++ API Poetry/API_PoetryApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_PoetryApp.swift
+//  API Poetry
+//
+//  Created by Maya Krishnan on 2/27/26.
+//
+
+import SwiftUI
+
+@main
+struct API_PoetryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
